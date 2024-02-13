df672c02f3c95288a801f3715a1e1129d0af4f08
Bekzat <kalibekzat2003@gmail.com>
2024-02-13 14:10:01 +0600

    some changes

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -14,7 +14,8 @@ const TodoForm = ({addTodo}) => {
 
   return (
     <form className='flex justify-between gap-3 mb-3 forMedia' onSubmit={handleSubmit}>
-      <input className='text-white bg-transparent border-2 border-white px-8 py-2 rounded-md' type='text' value={value} 
+      <input className='text-white bg-transparent border-2 border-white px-8 py-2 rounded-md' 
+      type='text' value={value} 
       placeholder='Write your task' 
       onChange={(e) => setValue(e.target.value)}/>
       <button className='text-center bg-purple-700 min-w-[106px] py-4 rounded-md text-cyan-50' type='submit'>Add a task</button>
